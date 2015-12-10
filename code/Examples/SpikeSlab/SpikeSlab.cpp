166489b498960f289291aa4ab5089786e63e7c56
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-12-10 16:00:45 +1300

    Working on SpikeSlab

---
+++ code/Examples/SpikeSlab/SpikeSlab.cpp
@@ -0,0 +1,56 @@
+#include "SpikeSlab.h"
+#include "Utils.h"
+
+using namespace std;
+using namespace DNest4;
+
+SpikeSlab::SpikeSlab()
+:params(20)
+{
+
+}
+
+void SpikeSlab::from_prior(RNG& rng)
+{
+	for(size_t i=0; i<params.size(); i++)
+		params[i] = -0.5 + rng.rand();
+}
+
+double SpikeSlab::perturb(RNG& rng)
+{
+	int which = rng.rand_int(params.size());
+	params[which] += rng.randh();
+	wrap(params[which], -0.5, 0.5);
+	return 0.;
+}
+
+double SpikeSlab::log_likelihood() const
+{
+	double u = 0.01;
+	double v = 0.1;
+	double C = log(1.0/sqrt(2*M_PI));
+
+	double logl1 = params.size()*(C - log(u));
+	double logl2 = params.size()*(C - log(v));
+
+	for(const double& x: params)
+	{
+		logl1 += -0.5*pow((x - 0.031)/u, 2);
+		logl2 += -0.5*pow(x/v, 2);
+	}
+	logl1 += log(100.);
+
+	return logsumexp(logl1, logl2);
+}
+
+void SpikeSlab::print(std::ostream& out) const
+{
+	for(const double& x: params)
+		out<<x<<' ';
+}
+
+string SpikeSlab::description() const
+{
+	return string("Each column is one of the 20 parameters.");
+}
+
---
+++ code/Examples/SpikeSlab/SpikeSlab.h
@@ -0,0 +1,34 @@
+#ifndef DNest4_SpikeSlab
+#define DNest4_SpikeSlab
+
+#include "RNG.h"
+#include <valarray>
+#include <ostream>
+
+class SpikeSlab
+{
+	private:
+		std::valarray<double> params;
+
+	public:
+		// Constructor only gives size of params
+		SpikeSlab();
+
+		// Generate the point from the prior
+		void from_prior(DNest4::RNG& rng);
+
+		// Metropolis-Hastings proposals
+		double perturb(DNest4::RNG& rng);
+
+		// Likelihood function
+		double log_likelihood() const;
+
+		// Print to stream
+		void print(std::ostream& out) const;
+
+		// Return string with column information
+		std::string description() const;
+};
+
+#endif
+
