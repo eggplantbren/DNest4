66396a0c442005297b0d3952a55f06e34084528c
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-12-10 14:55:56 +1300

    Utils

---
+++ code/Utils.h
@@ -0,0 +1,56 @@
+#ifndef DNest4_Utils
+#define DNest4_Utils
+
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+
+// Useful functions, copied from DNest3
+
+namespace DNest4
+{
+
+double mod(double y, double x);
+int mod(int y, int x);
+void wrap(double& x, double min, double max);
+double logsumexp(double* logv, int n);
+double logsumexp(const std::vector<double>& logv);
+double logsumexp(double a, double b);
+double logdiffexp(double a, double b);
+
+// Argsort from
+// http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
+template <typename T>
+std::vector<size_t> argsort(const std::vector<T>& v)
+{
+	// initialize original index locations
+	std::vector<size_t> idx(v.size());
+	for(size_t i=0; i<idx.size(); i++)
+		idx[i] = i;
+
+	// sort indexes based on comparing values in v
+	std::sort(idx.begin(), idx.end(),
+		[&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+
+	return idx;
+}
+
+// Calculate ranks
+template <typename T>
+std::vector<size_t> ranks(const std::vector<T>& v)
+{
+	// initialize original index locations
+	std::vector<size_t> ii = argsort(v);
+	std::vector<size_t> r(ii.size());
+
+	for(size_t i=0; i<ii.size(); i++)
+		r[ii[i]] = i;
+	return r;
+}
+
+} // namespace DNest4
+
+#include "UtilsImpl.h"
+#endif
+
---
+++ code/UtilsImpl.h
@@ -0,0 +1,71 @@
+namespace DNest4
+{
+
+double mod(double y, double x)
+{
+	assert(x > 0.);
+	return (y/x - floor(y/x))*x;
+}
+
+void wrap(double& x, double min, double max)
+{
+	x = DNest4::mod(x - min, max - min) + min;
+}
+
+int mod(int y, int x)
+{
+	assert(x > 0);
+	if(y >= 0)
+		return y - (y/x)*x;
+	else
+		return (x-1) - DNest4::mod(-y-1, x);
+}
+
+double logsumexp(double* logv, int n)
+{
+	assert(n > 1);
+	double max = logv[0];
+	for(int i=1; i<n; i++)
+		if(logv[i] > max)
+			max = logv[i];
+	double answer = 0;
+	// log(sum(exp(logf)) 	= log(sum(exp(logf - max(logf) + max(logf)))
+	//			= max(logf) + log(sum(exp(logf - max(logf)))
+	for(int i=0; i<n; i++)
+		answer += exp(logv[i] - max);
+	answer = max + log(answer);
+	return answer;
+}
+
+double logsumexp(const std::vector<double>& logv)
+{
+	int n = static_cast<int>(logv.size());
+	//if(n<=1)
+	//	cout<<"Warning in logsumexp"<<endl;
+	double max = *max_element(logv.begin(), logv.end());
+	double answer = 0;
+	// log(sum(exp(logf)) 	= log(sum(exp(logf - max(logf) + max(logf)))
+	//			= max(logf) + log(sum(exp(logf - max(logf)))
+	for(int i=0; i<n; i++)
+		answer += exp(logv[i] - max);
+	answer = max + log(answer);
+	return answer;
+}
+
+double logsumexp(double a, double b)
+{
+	double x[2] = {a,b};
+	return DNest4::logsumexp(x, 2);
+}
+
+double logdiffexp(double a, double b)
+{
+	assert(a > b);
+	double biggest = a;
+	a -= biggest;
+	b -= biggest;
+	return log(exp(a) - exp(b)) + biggest;
+}
+
+} // namespace DNest4
+
