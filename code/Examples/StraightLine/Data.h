bcad219f07b99a8b66d81a1d2bcdf62cc612eb40
Brendon Brewer <brendon.brewer@gmail.com>
2016-01-04 12:19:55 +1300

    Starting data class for StraightLine example

---
+++ code/Examples/StraightLine/Data.h
@@ -0,0 +1,31 @@
+#ifndef DNest4_Data
+#define DNest4_Data
+
+#include <valarray>
+
+/*
+* An object of this class is a dataset
+*/
+class Data
+{
+	private:
+		// The data points
+		std::valarray<double> x;
+		std::valarray<double> y;
+
+	public:
+		// Constructor
+		Data();
+
+		// Load data from a file
+		void load(const char* filename);
+
+		// Access to the data points
+		const std::valarray<double>& get_x() const
+		{ return x; }
+		const std::valarray<double>& get_y() const
+		{ return y; }
+};
+
+#endif
+
