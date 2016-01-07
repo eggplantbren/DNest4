ac3deab39cd84ba86e14642e8837863dcf21a55e
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-01-07 15:44:25 +1300

    more porting

---
+++ code/RJObject/RJObject.h
@@ -0,0 +1,110 @@
+#ifndef DNest4_RJObject
+#define DNest4_RJObject
+
+#include <vector>
+#include <ostream>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include "Utils.h"
+
+#include "RNG.h"
+
+namespace DNest4
+{
+
+/*
+* A class that implements basic birth-death Metropolis-Hastings
+* proposals using an exponential prior on the masses and a
+* uniform prior on the positions. This is designed for 1, 2,
+* or 3-dimensional objects. This class stores masses for
+* each of the components it has, but it does not store
+* any other properties. If your components have more properties
+* you may derive from this class or store them externally.
+*/
+
+template<class Distribution>
+class RJObject
+{
+	protected:
+		// How many parameters for each component
+		int num_dimensions;
+
+		// Maximum number of components allowed (minimum is zero)
+		int max_num_components;
+
+		// fixed num_components = max_num_components?
+		bool fixed;
+
+		// The hyperparameters that specify the conditional prior
+		// for the components
+		Distribution dist;
+
+		// The components
+		int num_components;
+		std::vector< std::vector<double> > components;
+
+		// Transformed into iid U(0, 1) priors
+		std::vector< std::vector<double> > u_components;
+
+		// Added/removed components
+		std::vector< std::vector<double> > added;
+		std::vector< std::vector<double> > removed;
+
+		// Helper methods -- these do one thing at a time
+		double perturb_num_components(RNG& rng, double scale);
+		double perturb_components(RNG& rng, double chance);
+
+		// Helper methods -- add or remove single component
+		double add_component(RNG& rng);
+		double remove_component(RNG& rng);
+
+	public:
+		/*
+		* num_dimensions: number of dimensions for each object.
+		* "mass" etc count. E.g. in StarField problem (x, y, flux)
+		* specifies a star, so num_dimensions = 3.
+		*
+		* max_num_components: obvious
+		* fixed: if true, doesn't do RJ steps. N will be fixeded at
+		* max_num_components
+		*/
+		RJObject(int num_dimensions, int max_num_components, bool fixed,
+				const Distribution& dist);
+
+		// Generate everything from the prior
+		void from_prior(RNG& rng);
+
+		// The top-level perturb method
+		double perturb(RNG& rng);
+
+		// Take "removed" components and put them into "added" with
+		// negative amplitude. Only applicable to certain models!
+		void consolidate_diff();
+
+		// For output
+		void print(std::ostream& out) const;
+
+		// Getter for components
+		const std::vector< std::vector<double> >& get_components() const
+		{ return components; }
+
+		// Getters for the diff
+		const std::vector< std::vector<double> >& get_added() const
+		{ return added; }
+
+		// Getters for the diff
+		const std::vector< std::vector<double> >& get_removed() const
+		{ return removed; }
+
+		// Getter for hyperparameters
+		const Distribution& get_dist() const
+		{ return dist; }
+};
+
+#include "RJObjectImpl.h"
+
+} // namespace DNest4
+
+#endif
+
---
+++ code/RJObject/RJObjectImpl.h
@@ -0,0 +1,250 @@
+template<class Distribution>
+RJObject<Distribution>::RJObject(int num_dimensions, int max_num_components, bool fixed,
+				const Distribution& dist)
+:num_dimensions(num_dimensions)
+,max_num_components(max_num_components)
+,fixed(fixed)
+,dist(dist)
+,num_components(0)
+{
+
+}
+
+template<class Distribution>
+void RJObject<Distribution>::from_prior(RNG& rng)
+{
+	added.resize(0);
+	removed.resize(0);
+
+	// Generate the hyperparameters from their prior
+	dist.from_prior();
+
+	int num = max_num_components;
+	// Generate from {0, 1, 2, ..., max_num_components}
+	if(!fixed)
+		num = rng.rand_int(max_num_components + 1);
+
+	// Resize the vectors of component properties
+	components.resize(0);
+	u_components.resize(0);
+
+	// Generate components
+	num_components = 0;
+	for(int i=0; i<num; i++)
+		add_component();
+}
+
+template<class Distribution>
+double RJObject<Distribution>::perturb_components(RNG& rng, double chance)
+{
+	if(num_components == 0)
+		return 0.;
+
+	// Enforce P(change only one) >~ 0.5
+	if(rng.rand() <= 0.5)
+		chance = 0.;
+
+	// A flag for whether each component gets changed or not
+	std::vector<bool> change(num_components, false);
+	int count = 0;
+	for(int i=0; i<num_components; i++)
+	{
+		if(rng.rand() <= chance)
+		{
+			change[i] = true;
+			count++;
+		}
+	}
+	// At least do one...
+	if(count == 0)
+		change[rng.rand_int(num_components)] = true;
+
+	for(int i=0; i<num_components; i++)
+	{
+		if(change[i])
+		{
+			// Accumulate added/removed component info
+			removed.push_back(components[i]);
+
+			// Perturb
+			for(int j=0; j<num_dimensions; j++)
+			{
+				u_components[i][j] += rng.randh();
+				u_components[i][j] = mod(
+							u_components[i][j], 1.);
+				components[i][j] = u_components[i][j];
+			}
+			dist.from_uniform(components[i]);
+
+			// Accumulate added/removed component info
+			added.push_back(components[i]);
+		}
+	}
+
+	return 0.;
+}
+
+template<class Distribution>
+double RJObject<Distribution>::add_component(RNG& rng)
+{
+	if(num_components >= max_num_components)
+	{
+		std::cerr<<"# WARNING: Trying to add component ";
+		std::cerr<<"but already full!"<<std::endl;
+		return 0.;
+	}
+
+	// Increment counter
+	num_components++;
+
+	// Generate component
+	std::vector<double> component(num_dimensions);
+	for(int j=0; j<num_dimensions; j++)
+		component[j] = rng.rand();
+	u_components.push_back(component);
+	dist.from_uniform(component);
+	components.push_back(component);
+
+	// Accumulate added/removed component info
+	added.push_back(component);
+
+	return 0.;
+}
+
+template<class Distribution>
+double RJObject<Distribution>::perturb_num_components(RNG& rng, double scale)
+{
+	double logH = 0.;
+
+	// Work out how many components we will have after the change
+	double delta = max_num_components*scale*rng.randn();
+	int difference = (int)delta;
+	// In case difference is zero, make it +1 or -1
+	if(difference == 0)
+	{
+		if(rng.rand() <= 0.5)
+			difference = -1;
+		else
+			difference =  1;
+	}
+	int new_num_components = DNest4::mod(num_components + difference, max_num_components + 1);
+	difference = new_num_components - num_components;
+
+	// Now do the required changes
+	if(difference > 0)
+	{
+		for(int i=0; i<difference; i++)
+			logH += add_component();
+	}
+	else
+	{
+		for(int i=0; i<-difference; i++)
+			logH += remove_component();
+	}
+
+	return logH;
+}
+
+template<class Distribution>
+double RJObject<Distribution>::perturb(RNG& rng)
+{
+	if(max_num_components == 0)
+		return 0.;
+
+	added.resize(0);
+	removed.resize(0);
+
+	double logH = 0.;
+
+	int which = (fixed)?(1 + rng.rand_int(2)):(rng.rand_int(3));
+
+	if(which == 0)
+	{
+		// Do some birth or death
+		logH += perturb_num_components(
+				pow(10., 1.5 - 6.*rng.rand()));
+	}
+	else if(which == 1)
+	{
+		// Change the hyperparameters
+		if(rng.rand() <= 0.5)
+		{
+			logH += dist.perturb1(components, u_components);
+		}
+		else
+		{
+			removed = components;
+			logH += dist.perturb2(components, u_components);
+			added = components;
+		}
+	}
+	else if(which == 2)
+	{
+		logH += perturb_components(pow(10., 0.5 - 4.*rng.rand()));
+	}
+
+	return logH;
+}
+
+template<class Distribution>
+double RJObject<Distribution>::remove_component(RNG& rng)
+{
+	if(num_components <= 0)
+	{
+		std::cerr<<"# WARNING: Trying to remove component ";
+		std::cerr<<"but already empty!"<<std::endl;
+		return 0.;
+	}
+
+	// Find one to delete
+	int i = rng.rand_int(num_components);
+
+	// Accumulate added/removed component info
+	removed.push_back(components[i]);
+
+	// Delete it
+	u_components.erase(u_components.begin() + i);
+	components.erase(components.begin() + i);
+
+	// Decrement counter
+	num_components--;
+
+	return 0.;
+}
+
+template<class Distribution>
+void RJObject<Distribution>::print(std::ostream& out) const
+{
+	out<<num_dimensions<<' '<<max_num_components<<' ';
+	dist.print(out); out<<' ';
+	out<<num_components<<' ';
+
+	// Write out components
+	for(int j=0; j<num_dimensions; j++)
+	{
+		for(int i=0; i<num_components; i++)
+			out<<components[i][j]<<' ';
+
+		// Pad with zeros (turned-off components)
+		for(int i=num_components; i<max_num_components; i++)
+			out<<0.<<' ';
+	}
+}
+
+template<class Distribution>
+void RJObject<Distribution>::consolidate_diff()
+{
+	if(Distribution::weight_parameter == -1)
+	{
+		std::cerr<<"# WARNING: consolidate_diff() failed."<<std::endl;
+		return;
+	}
+
+	for(size_t i=0; i<removed.size(); i++)
+	{
+		removed[i][Distribution::weight_parameter] *= -1;
+		added.push_back(removed[i]);
+	}
+	removed.clear();
+}
+
