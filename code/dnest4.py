743f791e1b8a217b995d9087343c81459e244365
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-01-13 13:47:06 +1300

    Plots

--- code/dnest4.py
+++ code/dnest4.py
@@ -218,3 +218,37 @@ def postprocess(temperature=1., numResampleLogX=1, plot=True, loaded=[], \
 
 	return [logz_estimate, H_estimate, logx_samples]
 
+
+def diffusion_plot():
+	"""
+	Plot a nice per-particle diffusion plot.
+	"""
+
+	sample_info = np.atleast_2d(np.loadtxt('sample_info.txt'))
+	ID = sample_info[:,3].astype('int')
+	j = sample_info[:,0].astype('int')
+
+	ii = np.arange(1, sample_info.shape[0] + 1)
+
+	for i in range(0, ID.max() + 1):
+		which = np.nonzero(ID == i)[0]
+		plt.plot(ii[which], j[which])
+
+	plt.xlabel('Iteration')
+	plt.ylabel('Level')
+	plt.show()
+
+def levels_plot():
+	"""
+	Plot the differences between the logl values of the levels.
+	"""
+	levels = np.loadtxt('levels.txt')
+
+	plt.plot(np.log10(np.diff(levels[:,1])))
+	plt.ylim([-1, 4])
+	plt.axhline(0., color='r')
+	plt.axhline(np.log10(np.log(10.)), color='g')
+	plt.xlabel('Level')
+	plt.ylabel('Delta log likelihood')
+	plt.show()
+
