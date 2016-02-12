531266473af5b32c28e6d3ece08cd8b0a11db94e
Dan F-M <danfm@nyu.edu>
2016-02-13 08:58:32 +1300

    demo

--- python/dnest4/model/PyModel.cpp
+++ python/dnest4/model/PyModel.cpp
@@ -11,33 +11,16 @@ PyModel::PyModel()
 void PyModel::from_prior(RNG& rng)
 {
     // Naive diffuse prior
-    m = rng.randn();
-
-    // Log-uniform prior
-    sigma = 1.0;  // exp(-10. + 20.*rng.rand());
+    m = 10*rng.randn();
 }
 
 double PyModel::perturb(RNG& rng)
 {
     double log_H = 0.;
 
-    // Proposals explore the prior
-    // For normal priors I usually use the hastings factor to do it
-    // int which = rng.rand_int(2);
-    // if(which == 0)
-    // {
-        log_H -= -0.5*pow(m/1E3, 2);
-        m += 1E3*rng.randh();
-        log_H += -0.5*pow(m/1E3, 2);
-    // }
-    // else
-    // {
-    //     // Usual log-uniform prior trick
-    //     sigma = log(sigma);
-    //     sigma += 20.*rng.randh();
-    //     wrap(sigma, -10., 10.);
-    //     sigma = exp(sigma);
-    // }
+    log_H -= -0.5*pow(m/10, 2);
+    m += 10*rng.randh();
+    log_H += -0.5*pow(m/10, 2);
 
     return log_H;
 }
@@ -53,7 +36,7 @@ double PyModel::log_likelihood() const
 
 void PyModel::print(std::ostream& out) const
 {
-    out<<m<<' '<<sigma;
+    out<<m;
 }
 
 string PyModel::description() const
