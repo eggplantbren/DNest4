a02a7b4f07af8553a0b3ddb5404d0d900f807c56
Dan F-M <danfm@nyu.edu>
2016-02-12 23:42:44 +1300

    python iterator

--- python/dnest4/model/PyModel.cpp
+++ python/dnest4/model/PyModel.cpp
@@ -5,15 +5,16 @@ using namespace DNest4;
 
 PyModel::PyModel()
 {
+    sigma = 1.0;
 }
 
 void PyModel::from_prior(RNG& rng)
 {
     // Naive diffuse prior
-    m = 1E3*rng.randn();
+    m = rng.randn();
 
     // Log-uniform prior
-    sigma = exp(-10. + 20.*rng.rand());
+    sigma = 1.0;  // exp(-10. + 20.*rng.rand());
 }
 
 double PyModel::perturb(RNG& rng)
@@ -22,21 +23,21 @@ double PyModel::perturb(RNG& rng)
 
     // Proposals explore the prior
     // For normal priors I usually use the hastings factor to do it
-    int which = rng.rand_int(2);
-    if(which == 0)
-    {
+    // int which = rng.rand_int(2);
+    // if(which == 0)
+    // {
         log_H -= -0.5*pow(m/1E3, 2);
         m += 1E3*rng.randh();
         log_H += -0.5*pow(m/1E3, 2);
-    }
-    else
-    {
-        // Usual log-uniform prior trick
-        sigma = log(sigma);
-        sigma += 20.*rng.randh();
-        wrap(sigma, -10., 10.);
-        sigma = exp(sigma);
-    }
+    // }
+    // else
+    // {
+    //     // Usual log-uniform prior trick
+    //     sigma = log(sigma);
+    //     sigma += 20.*rng.randh();
+    //     wrap(sigma, -10., 10.);
+    //     sigma = exp(sigma);
+    // }
 
     return log_H;
 }
