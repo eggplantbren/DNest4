3770b23bd1dcf7dc5d77c1ee287dba26e5bc5af6
Dan F-M <danfm@nyu.edu>
2016-02-12 17:02:01 +1300

    starting Python wrapper

---
+++ python/README.rst
@@ -0,0 +1,8 @@
+Building
+--------
+
+On Mac, you might need to do something like:
+
+.. code-block:: bash
+
+    MACOSX_DEPLOYMENT_TARGET=10.9 python setup.py build_ext --inplace
---
+++ python/dnest4/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1.dev0"
+
+try:
+    __DNEST4_SETUP__
+except NameError:
+    __DNEST4_SETUP__ = False
+
+if not __DNEST4_SETUP__:
+    __all__ = []
