c22a1683db302d475171fa4e3f86292de71ec21d
Dan F-M <danfm@nyu.edu>
2016-02-16 09:40:16 +1300

    hacking on analysis code

--- python/demo.py
+++ python/demo.py
@@ -4,6 +4,9 @@ import numpy as np
 
 class Model(object):
 
+    def __len__(self):
+        return 1
+
     def from_prior(self):
         return np.random.randn(1)
 
@@ -17,8 +20,19 @@ class Model(object):
         return -0.5*(np.sum(coords**2) + np.log(2*np.pi))
 
 
+num_particles = 10
+num_steps = 500
+
 model = Model()
-for sample in dnest4.sample(model, 100, num_particles=10):
-    print(sample["levels"])
-    print(sample["particles"])
-    assert 0
+samples = []
+sample_info = []
+
+for i, sample in enumerate(dnest4.sample(model, 100, num_steps=num_steps,
+                                         num_per_step=10000,
+                                         num_particles=num_particles)):
+    levels = sample["levels"]
+    samples.append(sample["samples"])
+    sample_info.append(sample["sample_info"])
+
+    if i > 100:
+        dnest4.postprocess(levels, np.array(samples), np.array(sample_info))
--- python/dnest4/__init__.py
+++ python/dnest4/__init__.py
@@ -8,6 +8,7 @@ except NameError:
     __DNEST4_SETUP__ = False
 
 if not __DNEST4_SETUP__:
-    __all__ = ["sample"]
+    __all__ = ["sample", "postprocess"]
 
     from ._dnest4 import sample
+    from .analysis import postprocess
