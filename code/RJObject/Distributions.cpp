25e1974106f062048dd629840f9ede740a54808e
Brendon Brewer <brendon.brewer@gmail.com>
2016-02-19 15:38:35 +1300

    Cauchy functions

--- code/RJObject/Distributions.cpp
+++ code/RJObject/Distributions.cpp
@@ -16,6 +16,7 @@ int sign(double x)
 
 double laplacian_log_pdf(double x, double center, double width)
 {
+	assert(width > 0.);
 	return -log(2.*width) - std::abs(x - center)/width;
 }
 
@@ -30,8 +31,26 @@ double laplacian_cdf_inverse(double x, double center,
 {
 	assert(width > 0.);
 	assert(x >= 0. && x <= 1.);
-
 	return center - width*sign(x - 0.5)*log(1. - 2.*std::abs(x - 0.5));
+}
+
+double cauchy_log_pdf(double x, double center, double width)
+{
+	assert(width > 0.);
+	return -log(M_PI) - log(width) - log(1. + pow((x - center)/width, 2));
+}
+
+double cauchy_cdf(double x, double center, double width)
+{
+	assert(width > 0.);
+	return atan((x - center)/width)/M_PI + 0.5;
+}
+
+double cauchy_cdf_inverse(double x, double center, double width)
+{
+	assert(width > 0.);
+	assert(x >= 0. && x <= 1.);
+	return center + width*tan(M_PI*(x - 0.5));
 }
 
 } // namespace DNest4
