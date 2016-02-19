a911d3a38ba78b7c684a600e9209197cdcb1823d
Dan F-M <danfm@nyu.edu>
2016-02-19 16:32:22 +1300

    removing old comments

--- python/dnest4/deprecated.py
+++ python/dnest4/deprecated.py
@@ -21,8 +21,8 @@ def postprocess(temperature=1., numResampleLogX=1, plot=True, loaded=[], \
 		levels_orig = np.atleast_2d(np.loadtxt("levels.txt"))
 		sample_info = np.atleast_2d(np.loadtxt("sample_info.txt"))
 		sample = np.atleast_2d(np.loadtxt("sample.txt"))
-		#if(sample.shape[0] == 1):
-		#	sample = sample.T
+		if(sample.shape[0] == 1):
+			sample = sample.T
 	else:
 		levels_orig, sample_info, sample = loaded[0], loaded[1], loaded[2]
 
