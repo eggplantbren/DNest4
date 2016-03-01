25429d071147b83fcae9f10acf0a2f1558b736df
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-03-02 12:25:02 +1300

    RJObject class

--- python/dnest4/rjobject.py
+++ python/dnest4/rjobject.py
@@ -4,4 +4,34 @@ import numpy.random as rng
 __all__ = "RJObject"
 
 
+class RJObject:
+    """
+    An RJObject is a collection of N objects where N is unknown
+    and the objects' properties may have a hierarchically-specified
+    prior distribution.
+    """
+
+    def __init__(self, N_max, dims, conditional_prior):
+        """
+        N_max (default=10): The maximum number of components allowed
+        dims = dimensionality of the parameter space of a component
+        conditional_prior = ConditionalPrior object
+        """
+        self.N_max = N_max
+        self.dims = dims
+        self.components = rng.rand(self.N_max, dims)
+        self.conditional_prior = conditional_prior
+
+    def from_prior(self):
+        """
+        Generate hyperparameters and components from the prior
+        """
+        self.components = rng.rand(self.N_max, dims)
+        self.conditional_prior.from_prior()
+
+    def perturb(self):
+        """
+        Metropolis-Hastings proposal
+        """
+        pass
 
