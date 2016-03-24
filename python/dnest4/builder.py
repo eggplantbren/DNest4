9553adafe25904e39836163e0c4c1f2a6eaa26b1
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-03-24 16:01:57 +1300

    more work

--- python/dnest4/builder.py
+++ python/dnest4/builder.py
@@ -3,18 +3,18 @@ class Uniform:
         self.a, self.b = a, b
 
     def from_prior(self):
-        s = r"{x} = {a} + ({b} - {a})*rng.rand();\n"
+        s = r"{x} = {a} + ({b} - ({a}))*rng.rand();\n"
         return self.insert_parameters(s)
 
     def perturb(self):
-        s  = r"{x} += ({b} - {a})*rng.randh();\n"
+        s  = r"{x} += ({b} - ({a}))*rng.randh();\n"
         s += r"wrap({x}, {a}, {b});\n"
         return self.insert_parameters(s)
 
     def log_density(self):
-        s  = r"if({x} < {a} || {x} > {b})\n"
+        s  = r"if({x} < ({a}) || {x} > ({b}))\n"
         s += r"    logp = -numeric_limits<double>::max();\n"
-        s += r"logp += -log({b} - {a});\n"
+        s += r"logp += -log({b} - ({a}));\n"
         return self.insert_parameters(s)
 
     def insert_parameters(self, s):
@@ -31,14 +31,14 @@ class Normal:
         return self.insert_parameters(s)
 
     def perturb(self):
-        s  = r"log_H -= -0.5*pow(({x} - {mu})/{sigma}, 2);\n"
-        s += r"{x} += {sigma}*rng.randh();\n"
-        s += r"log_H += -0.5*pow(({x} - {mu})/{sigma}, 2);\n"
+        s  = r"log_H -= -0.5*pow(({x}) - ({mu}))/({sigma}), 2);\n"
+        s += r"{x} += ({sigma})*rng.randh();\n"
+        s += r"log_H += -0.5*pow((({x}) - ({mu}))/({sigma}), 2);\n"
         return self.insert_parameters(s)
 
     def log_density(self):
         s  = r"logp += -log(2*M_PI) - log({sigma}) "
-        s += r"- 0.5*pow(({x} - {mu})/{sigma}, 2);\n"
+        s += r"- 0.5*pow((({x}) - ({mu}))/({sigma}), 2);\n"
         return self.insert_parameters(s)
 
     def insert_parameters(self, s):
@@ -47,9 +47,10 @@ class Normal:
         return s
 
 class Node:
-    def __init__(self, name, prior):
+    def __init__(self, name, prior, observed=False):
         self.name = name
         self.prior = prior
+        self.observed = observed
 
     def from_prior(self):
         return self.prior.from_prior().replace("{x}", self.name)
@@ -65,7 +66,5 @@ class Node:
 
 if __name__ == "__main__":
     theta = Node("theta", Uniform(-10.0, 10.0))
-    x = Node("x", Normal(theta, 1.0))
-
-    print(x.log_density())
+    x = Node("x", Normal(theta, 1.0), observed=True)
 
