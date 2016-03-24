c87af320e71f12e304cf7a784a892121f4beffb2
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-03-24 15:48:29 +1300

    more methods

--- python/dnest4/builder.py
+++ python/dnest4/builder.py
@@ -3,12 +3,47 @@ class Uniform:
         self.a, self.b = a, b
 
     def from_prior(self):
-        s = "{x} = {a} + ({b} - {a})*rng.rand();"
+        s = r"{x} = {a} + ({b} - {a})*rng.rand();\n"
+        return self.insert_parameters(s)
+
+    def perturb(self):
+        s  = r"{x} += ({b} - {a})*rng.randh();\n"
+        s += r"wrap({x}, {a}, {b});\n"
+        return self.insert_parameters(s)
+
+    def log_density(self):
+        s  = r"if({x} < {a} || {x} > {b})\n"
+        s += r"    logp = -numeric_limits<double>::max();\n"
+        s += r"logp += -log({b} - {a});\n"
         return self.insert_parameters(s)
 
     def insert_parameters(self, s):
         s = s.replace("{a}", str(self.a))
         s = s.replace("{b}", str(self.b))
+        return s
+
+class Normal:
+    def __init__(self, mu, sigma):
+        self.mu, self.sigma = mu, sigma
+
+    def from_prior(self):
+        s = r"{x} = {mu} + {sigma}*rng.randn();\n"
+        return self.insert_parameters(s)
+
+    def perturb(self):
+        s  = r"log_H -= -0.5*pow(({x} - {mu})/{sigma}, 2);\n"
+        s += r"{x} += {sigma}*rng.randh();\n"
+        s += r"log_H += -0.5*pow(({x} - {mu})/{sigma}, 2);\n"
+        return self.insert_parameters(s)
+
+    def log_density(self):
+        s  = r"logp += -log(2*M_PI) - log({sigma}) "
+        s += r"- 0.5*pow(({x} - {mu})/{sigma}, 2);\n"
+        return self.insert_parameters(s)
+
+    def insert_parameters(self, s):
+        s = s.replace("{mu}", str(self.mu))
+        s = s.replace("{sigma}", str(self.sigma))
         return s
 
 class Node:
@@ -19,13 +54,18 @@ class Node:
     def from_prior(self):
         return self.prior.from_prior().replace("{x}", self.name)
 
+    def perturb(self):
+        return self.prior.perturb().replace("{x}", self.name)
+
+    def log_density(self):
+        return self.prior.log_density().replace("{x}", self.name)
+
     def __str__(self):
         return self.name
 
-
 if __name__ == "__main__":
+    theta = Node("theta", Uniform(-10.0, 10.0))
+    x = Node("x", Normal(theta, 1.0))
 
-    theta = Node("theta", Uniform(0.0, 1.0))
-
-    print(theta.from_prior())
+    print(x.log_density())
 
