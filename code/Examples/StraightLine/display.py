e8e979542eb0633c2d9229c87c170e2f3e031f62
Brendon Brewer <brendon.brewer@gmail.com>
2016-05-15 13:57:34 +1200

    New figure, moved command line options to an appendix

--- code/Examples/StraightLine/display.py
+++ code/Examples/StraightLine/display.py
@@ -8,19 +8,17 @@ xmin = data[:,0].min()
 xmax = data[:,0].max()
 
 x = linspace(xmin - 0.2*(xmax - xmin), xmax + 0.2*(xmax - xmin), 1001)
-ion()
 
+# Plot regression lines
 for i in range(posterior_sample.shape[0]):
-	hold(False)
-	plot(data[:,0], data[:,1], 'bo')
-	hold(True)
-	y = posterior_sample[i, 0]*x + posterior_sample[i, 1]
-	plot(x, y, 'k')
-	xlabel('$x$', fontsize=20)
-	ylabel('$y$', fontsize=20)
-	title('Model {k}/{N}'.format(k=(i+1), N=posterior_sample.shape[0]))
-	axis([0, 100, 0, 800])
-	draw()
-ioff()
+    y = posterior_sample[i, 0]*x + posterior_sample[i, 1]
+    plot(x, y, 'k', alpha=0.01)
+
+plot(data[:,0], data[:,1], 'bo')
+xlabel('$x$', fontsize=18)
+ylabel('$y$', fontsize=18)
+axis([0, 100, 0, 800])
+title('Regression Lines', fontsize=16)
+savefig('regression_lines.pdf', bbox_inches='tight')
 show()
 
