5253a3bd3f8a434aea0fc9521fbcf85bc385d2d2
Brendon Brewer <brendon.brewer@gmail.com>
2016-05-28 19:17:42 +1200

    Laplace distribution

---
+++ code/Distributions/Laplace.cpp
@@ -0,0 +1,44 @@
+#include "Laplace.h"
+#include <stdexcept>
+#include <cmath>
+#include "Utils.h"
+
+namespace DNest4
+{
+
+Laplace::Laplace(double center, double width)
+:center(center)
+,width(width)
+{
+    if(width <= 0.0)
+        throw std::domain_error("Laplace distribution must have positive width.");
+}
+
+double Laplace::cdf(double x) const
+{
+	return 0.5 + 0.5*sign(x - center)*(1.0 - exp(-std::abs(x - center)/width));
+}
+
+double Laplace::cdf_inverse(double x) const
+{
+    if(x < 0.0 || x > 1.0)
+        throw std::domain_error("Input to cdf_inverse must be in [0, 1].");
+	return center - width*sign(x - 0.5)*log(1.0 - 2*std::abs(x - 0.5));
+}
+
+double Laplace::log_pdf(double x) const
+{
+    return -log(2.0) - std::abs(x - center)/width;
+}
+
+int Laplace::sign(double x)
+{
+	if(x == 0.0)
+		return 0;
+	if(x > 0.0)
+		return 1;
+	return -1;
+}
+
+} // namespace DNest4
+
---
+++ code/Distributions/Laplace.h
@@ -0,0 +1,34 @@
+#ifndef DNest4_Laplace
+#define DNest4_Laplace
+
+#include "ContinuousDistribution.h"
+#include "../RNG.h"
+#include <limits>
+
+namespace DNest4
+{
+
+/*
+* Laplace distributions
+*/
+class Laplace:public ContinuousDistribution
+{
+    private:
+        // Location and scale parameter
+        double center, width;
+
+    public:
+        Laplace(double center=0.0, double width=1.0);
+
+        double cdf(double x) const;
+        double cdf_inverse(double x) const;
+        double log_pdf(double x) const;
+
+        // Sign function
+        static int sign(double x);
+};
+
+} // namespace DNest4
+
+#endif
+
