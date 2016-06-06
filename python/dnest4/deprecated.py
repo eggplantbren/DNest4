b6e9ebd324c8857d1c79cb5a36caaa633d2919d1
Brendon Brewer <brendon.brewer@gmail.com>
2016-06-06 17:27:25 -0400

    Import from loading

--- python/dnest4/deprecated.py
+++ python/dnest4/deprecated.py
@@ -2,6 +2,7 @@ import copy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from .loading import *
 
 def logsumexp(values):
 	biggest = np.max(values)
@@ -15,57 +16,6 @@ def logdiffexp(x1, x2):
 	xx2 = x2 - biggest
 	result = np.log(np.exp(xx1) - np.exp(xx2)) + biggest
 	return result
-
-def my_loadtxt(filename, single_precision=False):
-    if single_precision:
-        return pd.read_csv(filename, header=None, sep=' ',\
-                           comment="#", dtype=np.float32)\
-                                    .dropna(axis=1).values
-    return pd.read_csv(filename, header=None, sep=' ', comment="#")\
-                                                 .dropna(axis=1).values
-
-def loadtxt_rows(filename, rows, single_precision=False):
-    """
-    Load only certain rows
-    """
-    # Open the file
-    f = open(filename, "r")
-
-    # Storage
-    results = {}
-
-    # Row number
-    i = 0
-
-    # Number of columns
-    ncol = None
-
-    while(True):
-        # Read the line and split by whitespace
-        line = f.readline()
-        cells = line.split()
-
-        # Quit when you see a different number of columns
-        if ncol is not None and len(cells) != ncol:
-            break
-
-        # Non-comment lines
-        if cells[0] != "#":
-            # If it's the first one, get the number of columns
-            if ncol is None:
-                ncol = len(cells)
-
-            # Otherwise, include in results
-            if i in rows:
-                if single_precision:
-                    results[i] = np.array([float(cell) for cell in cells],\
-                                                              dtype="float32")
-                else:
-                    results[i] = np.array([float(cell) for cell in cells])
-            i += 1
-
-    results["ncol"] = ncol
-    return results
 
 
 def postprocess(temperature=1., numResampleLogX=1, plot=True, loaded=[], \
