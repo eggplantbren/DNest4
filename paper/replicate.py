10613fba22c3376d940dcbc0541178e16471544d
Brendon Brewer <brendon.brewer@gmail.com>
2016-07-03 20:05:21 +1200

    0.1.1

--- paper/replicate.py
+++ paper/replicate.py
@@ -3,7 +3,8 @@ import os
 # Python command on your system
 my_python = "/home/brendon/local/anaconda3/bin/python"
 
-print("This script should replicate all the runs presented in the paper.\nNote:\
+print("This script should download and install DNest4 and \
+replicate all the runs presented in the paper.\nNote:\
 plots will be generated, which need to be manually\n\
 closed for the script to continue. It assumes\n\
 everything has been compiled already.\n\
@@ -12,9 +13,9 @@ NOTE: the RJObject_1DMixture example is fairly time-consuming.\n\n\
 Press ENTER to begin.")
 junk = input()
 
-os.system("wget https://github.com/eggplantbren/DNest4/archive/0.1.0.tar.gz")
-os.system("tar xvzf 0.1.0.tar.gz")
-os.system("mv DNest4-0.1.0 DNest4")
+os.system("wget https://github.com/eggplantbren/DNest4/archive/0.1.1.tar.gz")
+os.system("tar xvzf 0.1.1.tar.gz")
+os.system("mv DNest4-0.1.1 DNest4")
 os.chdir("DNest4/code")
 os.system("make")
 os.chdir("../python")
