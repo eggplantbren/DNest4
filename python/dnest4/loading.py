3ce64f922e266d06eb225a85102024fbb88d80a2
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-07-12 11:49:41 +1200

    Removed pandas dependency, my_loadtxt works well

--- python/dnest4/loading.py
+++ python/dnest4/loading.py
@@ -1,17 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pandas as pd
 
 __all__ = ["my_loadtxt", "loadtxt_rows"]
 
-def my_loadtxt(filename, single_precision=False, delimiter=" "):
-    if single_precision:
-        return pd.read_csv(filename, header=None, delimiter=delimiter,\
-                           comment="#", dtype=np.float32)\
-                                    .dropna(axis=1).values
-    return pd.read_csv(filename, header=None, delimiter=delimiter, comment="#")\
-                                                 .dropna(axis=1).values
+def my_loadtxt(filename, single_precision=False):
+    """
+    Load quickly
+    """
+    # Open the file
+    f = open(filename, "r")
+
+    # Storage
+    results = []
+
+    # Rows and columns
+    nrow = 0
+    ncol = None
+
+    while(True):
+        # Read the line and split by whitespace
+        line = f.readline()
+        cells = line.split()
+
+        # Quit when you see a different number of columns
+        if ncol is not None and len(cells) != ncol:
+            break
+
+        # Non-comment lines
+        if cells[0][0] != "#":
+            # If it's the first one, get the number of columns
+            if ncol is None:
+                ncol = len(cells)
+
+            # Otherwise, include in results
+            if single_precision:
+                results.append(np.array([float(cell) for cell in cells],\
+                                                          dtype="float32"))
+            else:
+                results.append(np.array([float(cell) for cell in cells]))
+            nrow += 1
+
+    results = np.vstack(results)
+    return results
 
 def loadtxt_rows(filename, rows, single_precision=False):
     """
