6dfb69ea52fbdb25e797a6aa90685ddc905bbcf5
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-07-24 16:37:38 +1200

    Nicer plot

--- code/Examples/RJObject_SineWaves/make_plots.py
+++ code/Examples/RJObject_SineWaves/make_plots.py
@@ -5,17 +5,20 @@ rc("text", usetex=True)
 
 posterior_sample = loadtxt('posterior_sample.txt')
 
-hist(posterior_sample[:,1005], 50)
+width = 0.5
+bins = np.arange(0.0, 100.0) - 0.5*width
+
+hist(posterior_sample[:,1008], bins=bins, width=width, color="k", alpha=0.2)
 xlabel('Number of Sinusoids $N$', fontsize=14)
 ylabel('Number of posterior samples', fontsize=14)
-xlim([-0.5, 10.5])
+xlim([-0.5, 100.5])
 savefig('N_result.pdf', bbox_inches='tight')
 show()
 
-sample_info = loadtxt('Metropolis/sample_info.txt')
-plot(sample_info[:,1], linewidth=1)
-xlabel('Iteration / 20000')
-ylabel('Log Likelihood')
-savefig('trace_logl.pdf', bbox_inches='tight')
-show()
+#sample_info = loadtxt('Metropolis/sample_info.txt')
+#plot(sample_info[:,1], linewidth=1)
+#xlabel('Iteration / 20000')
+#ylabel('Log Likelihood')
+#savefig('trace_logl.pdf', bbox_inches='tight')
+#show()
 
