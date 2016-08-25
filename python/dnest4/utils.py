f68e8cb9751a32cc4d8bdc97c6f753395381e1e1
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-08-25 14:00:16 +1200

    Allow N > 1 randhs to be generated

--- python/dnest4/utils.py
+++ python/dnest4/utils.py
@@ -4,11 +4,14 @@ __all__ = ["randh", "wrap"]
 import numpy as np
 import numpy.random as rng
 
-def randh():
+def randh(N=1):
     """
     Generate from the heavy-tailed distribution.
     """
-    return 10.0**(1.5 - 3*np.abs(rng.randn()/np.sqrt(-np.log(rng.rand()))))*rng.randn()
+    if N==1:
+	    return 10.0**(1.5 - 3*np.abs(rng.randn()/np.sqrt(-np.log(rng.rand()))))*rng.randn()
+    return 10.0**(1.5 - 3*np.abs(rng.randn(N)/np.sqrt(-np.log(rng.rand(N)))))*rng.randn(N)
+
 
 def wrap(x, a, b):
     assert b > a
