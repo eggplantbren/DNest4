7c97bbe3e25f7cc8953fd286a0736ede09f97dcf
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-18 11:53:02 +1300

    Use existing code (don't clone again)

--- paper/replicate.py
+++ paper/replicate.py
@@ -1,25 +1,26 @@
 import os
+import matplotlib.pyplot
 
 # Best Python command on your system
-my_python = "python"
+my_python = "/home/brendon/local/anaconda3/bin/python"
 
-print("This script should download and install DNest4 and \
+print("This script should\
 replicate all the runs presented in the paper.\nNote:\
 plots will be generated, which need to be manually\n\
 closed for the script to continue. It assumes\n\
-everything has been compiled already.\n\
+everything has been compiled already as per the instructions in the paper.\n\
 \n\
 NOTE: the RJObject_1DMixture example is fairly time-consuming.\n\n\
 Press ENTER to begin.")
 junk = input()
 
-os.system("wget https://github.com/eggplantbren/DNest4/archive/0.1.3.tar.gz")
-os.system("tar xvzf 0.1.3.tar.gz")
-os.system("mv DNest4-0.1.3 DNest4")
-os.chdir("DNest4/code")
-os.system("make")
-os.chdir("../python")
-os.system("python setup.py install")
+#os.system("wget https://github.com/eggplantbren/DNest4/archive/0.1.3.tar.gz")
+#os.system("tar xvzf 0.1.3.tar.gz")
+#os.system("mv DNest4-0.1.3 DNest4")
+#os.chdir("DNest4/code")
+#os.system("make")
+#os.chdir("../python")
+#os.system("python setup.py install")
 
 def run_example(directory):
     os.chdir(directory)
