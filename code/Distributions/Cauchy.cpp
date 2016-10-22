7189b93aeae199bda877296115ffbbc527fe0058
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-23 11:15:04 +1300

    For slight speedup

--- code/Distributions/Cauchy.cpp
+++ code/Distributions/Cauchy.cpp
@@ -28,7 +28,7 @@ double Cauchy::cdf_inverse(double x) const
 
 double Cauchy::log_pdf(double x) const
 {
-    return -log(M_PI) - log(width) - log(1.0 + pow((x - center)/width, 2));
+    return -log(M_PI*width) - log(1.0 + pow((x - center)/width, 2));
 }
 
 } // namespace DNest4
