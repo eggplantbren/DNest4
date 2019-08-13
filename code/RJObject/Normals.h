85d7d07a27be8cdbf9887291ada6a5a42aae08cc
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-08-13 12:14:47 +1200

    fleshing out the class

--- code/RJObject/Normals.h
+++ code/RJObject/Normals.h
@@ -1,17 +1,51 @@
 #ifndef DNest4_Normals
 #define DNest4_Normals
 
+#include "../RNG.h"
+#include <vector>
+
 namespace DNest4
 {
 
 /* A template class for when I want a vector of quantities
-   with a normal prior given some hyperparameters. */
+   with a normal prior given some hyperparameters. The
+   priors for mu and sigma are independent. */
 
 template<class MuPrior, class SigmaPrior>
 class Normals
 {
+    private:
 
+        // These should be distribution objects
+        MuPrior mu_prior;
+        SigmaPrior sigma_prior;
 
+        // Values for the hyperparameters
+        double mu, sigma;
+
+        // Vectors of latent ns and produced ys
+        std::vector<double> ns, ys;
+
+        // Syncing functions
+        void sync_logp();
+        void sync_ys();
+
+        // log p(y | alpha)
+        double logp;
+
+    public:
+
+        // Constructor. Pass priors and size
+        Normals(MuPrior _mu_prior,
+                SigmaPrior _sigma_prior,
+                int size);
+
+        // The usual DNest4 functions
+        void from_prior(RNG& rng);
+        double perturb(RNG& rng);
+
+        // Getter
+        const std::vector<double>& get_ys() const;
 };
 
 } // namespace
--- code/RJObject/NormalsImpl.h
+++ code/RJObject/NormalsImpl.h
@@ -1,5 +1,53 @@
+#include <cassert>
+#include <cmath>
+
 namespace DNest4
 {
+
+template<class MuPrior, class SigmaPrior>
+Normals<MuPrior, SigmaPrior>::Normals(MuPrior _mu_prior,
+                                      SigmaPrior _sigma_prior,
+                                      int size)
+:mu_prior(std::move(_mu_prior))
+,sigma_prior(std::move(_sigma_prior))
+,ns(size)
+,ys(size)
+{
+    assert(size >= 1);
+}
+
+
+template<class MuPrior, class SigmaPrior>
+void Normals<MuPrior, SigmaPrior>::from_prior(RNG& rng)
+{
+    mu = mu_prior.generate(rng);
+    sigma = sigma_prior.generate(rng);
+
+    for(double& n: ns)
+        n = rng.randn();
+
+    sync_ys();
+    sync_logp();
+}
+
+template<class MuPrior, class SigmaPrior>
+void Normals<MuPrior, SigmaPrior>::sync_ys()
+{
+    for(size_t i=0; i<ns.size(); ++i)
+        ys[i] = mu + sigma*ns[i];
+}
+
+
+template<class MuPrior, class SigmaPrior>
+void Normals<MuPrior, SigmaPrior>::sync_logp()
+{
+    double C = -0.5*log(2.0*M_PI*sigma*sigma);
+    double tau = 1.0/(sigma*sigma);
+    logp = 0.0;
+    for(double y: ys)
+        logp += C - 0.5*tau*pow(y - mu, 2);
+}
+
 
 
 } // namespace
