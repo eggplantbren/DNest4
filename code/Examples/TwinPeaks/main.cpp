ef594c1e8cd14d721677b211d26bb95b78ae83ef
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-09-17 15:21:06 +1200

    mv -> cp

--- code/Examples/TwinPeaks/main.cpp
+++ code/Examples/TwinPeaks/main.cpp
@@ -13,18 +13,18 @@ int main(int argc, char** argv)
 	Sampler<MyModel> sampler = setup<MyModel>(cl_options);
 	sampler.run();
     system("/home/brewer/local/anaconda3/bin/python3 showresults.py");
-    system("mv sample.txt sample_first.txt");
-    system("mv sample_info.txt sample_info_first.txt");
-    system("mv log_prior_weights.txt log_prior_weights_first.txt");
+    system("cp sample.txt sample_first.txt");
+    system("cp sample_info.txt sample_info_first.txt");
+    system("cp log_prior_weights.txt log_prior_weights_first.txt");
 
     // Now run with second scalar
     MyModel::set_mode(Mode::second);
 	sampler = setup<MyModel>(cl_options);
 	sampler.run();
     system("/home/brewer/local/anaconda3/bin/python3 showresults.py");
-    system("mv sample.txt sample_second.txt");
-    system("mv sample_info.txt sample_info_second.txt");
-    system("mv log_prior_weights.txt log_prior_weights_second.txt");
+    system("cp sample.txt sample_second.txt");
+    system("cp sample_info.txt sample_info_second.txt");
+    system("cp log_prior_weights.txt log_prior_weights_second.txt");
 
     // Load the data from the first two runs and then target the sum
     MyModel::load_data();
@@ -32,9 +32,9 @@ int main(int argc, char** argv)
 	sampler = setup<MyModel>(cl_options);
 	sampler.run();
     system("/home/brewer/local/anaconda3/bin/python3 showresults.py");
-    system("mv sample.txt sample_final.txt");
-    system("mv sample_info.txt sample_info_final.txt");
-    system("mv log_prior_weights.txt log_prior_weights_final.txt");
+    system("cp sample.txt sample_final.txt");
+    system("cp sample_info.txt sample_info_final.txt");
+    system("cp log_prior_weights.txt log_prior_weights_final.txt");
 
     return 0;
 }
