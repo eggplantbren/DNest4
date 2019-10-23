f92f8a2aea2103c36959159bc317238ca8ccfacb
Dan F-M <foreman.mackey@gmail.com>
2019-10-23 10:00:15 -0400

    fixing build process

--- python/dnest4/__init__.py
+++ python/dnest4/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 try:
     __DNEST4_SETUP__
@@ -8,15 +8,16 @@ except NameError:
     __DNEST4_SETUP__ = False
 
 if not __DNEST4_SETUP__:
-    __all__ = ["DNest4Sampler", "postprocess", "analysis", "my_loadtxt, loadtxt_rows"]
+    __all__ = [
+        "DNest4Sampler",
+        "postprocess",
+        "analysis",
+        "my_loadtxt, loadtxt_rows",
+    ]
 
     from . import analysis
     from .sampler import DNest4Sampler
     from .deprecated import postprocess, postprocess_abc
     from .loading import my_loadtxt, loadtxt_rows
-    from .utils import rand
-    from .utils import randn
-    from .utils import randt2
-    from .utils import randh
-    from .utils import wrap
+    from .utils import rand, randn, randt2, randh, wrap
 
