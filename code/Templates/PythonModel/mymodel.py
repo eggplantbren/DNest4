349db3a42aaeb1476ee6af2b0898e7f4e1d22a56
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-09-05 14:58:18 +1200

    Put the normalising constant in so the true log(Z) is zero

--- code/Templates/PythonModel/mymodel.py
+++ code/Templates/PythonModel/mymodel.py
@@ -7,7 +7,7 @@ def prior_transform(us):
     return us - 0.5
 
 def log_likelihood(xs):
-    return -0.5*np.sum((xs/0.01)**2)
+    return -num_params*0.5*np.log(2*np.pi*0.01**2)-0.5*np.sum((xs/0.01)**2)
 
 
 def both(us):
