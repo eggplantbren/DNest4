628aea0a24d148badeceea46fcafe497f4299022
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-09-08 07:59:50 +1200

    params

--- code/Templates/PythonModel/mymodel.py
+++ code/Templates/PythonModel/mymodel.py
@@ -6,8 +6,8 @@ num_params = 10
 def prior_transform(us):
     return us - 0.5
 
-def log_likelihood(xs):
-    return -num_params*0.5*np.log(2*np.pi*0.01**2)-0.5*np.sum((xs/0.01)**2)
+def log_likelihood(params):
+    return -num_params*0.5*np.log(2*np.pi*0.01**2)-0.5*np.sum((params/0.01)**2)
 
 
 def both(us):
