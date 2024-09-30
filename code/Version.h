31d0eb60c19b3575170b0eb295e033a55b184f87
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-10-01 10:16:29 +1300

    Version bump

--- code/Version.h
+++ code/Version.h
@@ -2,8 +2,8 @@
 #define DNest4_Version
 
 #define DNEST4_MAJOR_VERSION 0
-#define DNEST4_MINOR_VERSION 2
-#define DNEST4_PATCH_VERSION 4
+#define DNEST4_MINOR_VERSION 3
+#define DNEST4_PATCH_VERSION 0
 
 #endif  // DNest4_Version
 
--- python/dnest4/__init__.py
+++ python/dnest4/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 try:
     __DNEST4_SETUP__
