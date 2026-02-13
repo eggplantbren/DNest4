12cd9967ec5433c335e7f43e055e7a83f4f30fa8
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-02-13 13:41:52 +1300

    Updated init.py

--- python/dnest4/__init__.py
+++ python/dnest4/__init__.py
@@ -1,23 +1,4 @@
-# -*- coding: utf-8 -*-
-
-__version__ = "0.3.3"
-
-try:
-    __DNEST4_SETUP__
-except NameError:
-    __DNEST4_SETUP__ = False
-
-if not __DNEST4_SETUP__:
-    __all__ = [
-        "DNest4Sampler",
-        "postprocess",
-        "analysis",
-        "my_loadtxt, loadtxt_rows",
-    ]
-
-    from . import analysis
-    from .sampler import DNest4Sampler
-    from .deprecated import postprocess, postprocess_abc
-    from .loading import my_loadtxt, loadtxt_rows
-    from .utils import rand, randn, randt2, randh, wrap
-
+from .builder import *
+from .classic import *
+from .distributions import *
+from .loading import *
